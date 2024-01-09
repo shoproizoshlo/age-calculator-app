bdd5aafca866a84877082d7571599961ba2a271b
shoproizoshlo <iamsusunny@gmail.com>
2024-01-09 11:53:33 +0100

    refactoring and solved current year calculation problem

--- js/script.js
+++ js/script.js
@@ -1,4 +1,150 @@
+const validations_options = [
+  // check if value pass year
+  {
+    attribute: "id",
+    isValid: (input) => {
+      input.id !== "year";
+      const today = new Date();
+      // calculate raw differences
+      return today.getFullYear() >= input.value;
+    },
+    errorMessage: () => `Must be in the past`,
+  },
+  // check if value pass minlength
+  {
+    attribute: "max",
+    isValid: (input) => {
+      return input.max >= input.value && input.min <= input.value;
+    },
+    errorMessage: (input) => `Must be a valid ${input.name}`,
+  },
+  // check if value pass required
+  {
+    attribute: "required",
+    isValid: (input) => input.value.trim() !== "",
+    errorMessage: () => `This field is required`,
+  },
+];
+
 const form = document.getElementById("form");
+
+const formElement = document.querySelector("#form");
+// disabling HTML standart validation
+formElement.setAttribute("novalidate", "");
+//adding our validation
+formElement.addEventListener("submit", (event) => {
+  // prevent page reload after submit
+  event.preventDefault();
+  // go to validation all inputs
+  validateAllFormGroups(formElement);
+});
+
+function validateAllFormGroups(formToValidate) {
+  const formGroups = Array.from(
+    formToValidate.querySelectorAll(".input-control")
+  );
+
+  // variable for check error for all forms together
+  let allFormGroupsValid = true;
+
+  //   loop through array
+  formGroups.forEach((formGroup) => {
+    validateSingleFormGroup(formGroup);
+
+    if (formGroup.querySelector(".error").textContent) {
+      allFormGroupsValid = false;
+    }
+  });
+
+  if (!allFormGroupsValid) return;
+
+  const input = document.querySelectorAll("input");
+  input.forEach((input) => {
+    input.classList.remove("border-danger-subtle");
+  });
+
+  const { years, months, days } = calculateAge();
+  displayAge({ years, months, days });
+}
+
+function validateSingleFormGroup(formGroup) {
+  // select input, label, error img and error class
+  const label = formGroup.querySelector("label");
+  const input = formGroup.querySelector("input");
+  const errorContainer = formGroup.querySelector(".error");
+
+  //   set no error to the input
+  let formGroupError = false;
+  // validation rules that loop through then check the input egainst each of rules and trigger right error message
+
+  const { inputError: optionsError, errorMessage: optionsErrorMessage } =
+    validateOptions(input, errorContainer);
+
+  if (optionsError) {
+    errorContainer.textContent = optionsErrorMessage;
+    errorStyling({ errorContainer, input, label });
+    formGroupError = true;
+  }
+
+  // Check for valid day based on the month
+  if (input.id === "day" && input.value) {
+    const { inputError: dateInputError, errorMessage: dateInputErrorMessage } =
+      validateDateInput(input);
+
+    if (dateInputError) {
+      errorContainer.textContent = dateInputErrorMessage;
+      errorStyling({ errorContainer, input, label });
+      formGroupError = true;
+    }
+  }
+
+  //   remove error border and icon from input if here is no error
+  if (!formGroupError) {
+    label.classList.remove("error-label");
+    errorContainer.textContent = "";
+  }
+}
+
+// helper functions
+//Sergii: I have extracted the functions outside and used function keyword instead of arrow function
+// so that they are hoisted and can be used before they are defined
+// you can read more here https://codecryrepeat.hashnode.dev/imagine-execution-context-and-hoisting (shameless plug)
+function validateOptions(input) {
+  //   set no error to the input
+  let inputError = false;
+  let errorMessage = "";
+  // validation rules that loop through then check the input egainst each of rules and trigger right error message
+  for (const option of validations_options) {
+    if (input.hasAttribute(option.attribute) && !option.isValid(input)) {
+      errorMessage = option.errorMessage(input);
+      inputError = true;
+    }
+  }
+
+  return {
+    inputError,
+    errorMessage,
+  };
+}
+
+function validateDateInput(input) {
+  const day = parseInt(input.value);
+  const month = parseInt(document.getElementById("month").value);
+
+  let errorMessage = "";
+  let inputError = false;
+
+  // Check if day is within valid range for the selected month
+  if (day < 1 || day > getDaysInMonth(month) || day > 31) {
+    errorMessage = "Must be a valid day";
+    inputError = true;
+  }
+
+  return {
+    inputError,
+    errorMessage,
+  };
+}
 
 function calculateAge() {
   const day = document.getElementById("day").value;
@@ -15,155 +161,42 @@ function calculateAge() {
   const months = ageDate.getUTCMonth();
   const days = ageDate.getUTCDate() - 1;
 
-  document.getElementById("years").innerText = `${years}`;
-  document.getElementById("months").innerText = `${months}`;
-  document.getElementById("days").innerText = `${days}`;
+  return { years, months, days };
 }
 
-// creating function that get form selector
-const validateForm = (formSelector) => {
-  const formElement = document.querySelector(formSelector);
+function displayAge({ years, months, days }) {
+  document.getElementById("years").innerText = years;
+  document.getElementById("months").innerText = months;
+  document.getElementById("days").innerText = days;
+}
 
-  // items in array have object and 3 properties
-  const validationOptions = [
-    // check if value pass year
-    {
-      attribute: "id",
-      isValid: (input) => {
-        input.id !== "year";
-        const today = new Date();
-        // calculate raw differences
-        return today.getFullYear() > input.value;
-      },
-      errorMessage: (input) => `Must be in the past`,
-    },
+function errorStyling({ errorContainer, input, label }) {
+  errorContainer.style.display = "block";
+  //   add red border around input
+  input.classList.add("border-danger-subtle");
 
-    // check if value pass minlength
-    {
-      attribute: "max",
-      isValid: (input) => {
-        return input.max >= input.value && input.min <= input.value;
-      },
-      errorMessage: (input) => `Must be a valid ${input.name}`,
-    },
+  //   change position for button
+  if (window.innerWidth <= 500) {
+    // styles for mobile devices
+    document.querySelector("button").style.top = "117px";
+  } else {
+    document.querySelector("button").style.top = "87px";
+  }
 
-    // check if value pass required
-    {
-      attribute: "required",
-      isValid: (input) => input.value.trim() !== "",
-      errorMessage: (input) => `This field is required`,
-    },
-  ];
+  label.classList.add("error-label");
+}
 
-  const validateSingleFormGroup = (formGroup) => {
-    // select input, label, error img and error class
-    const label = formGroup.querySelector("label");
-    const input = formGroup.querySelector("input");
-    const errorContainer = formGroup.querySelector(".error");
+// Function to get the number of days in a month, considering leap years
+function getDaysInMonth(month) {
+  const leapYear = (year) =>
+    (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    function errorStyling() {
-      errorContainer.style.display = "block";
-      //   add red border around input
-      input.classList.add("border-danger-subtle");
+  // Adjust February's days for leap years
+  if (month === 2) {
+    const year = parseInt(document.getElementById("year").value);
+    return leapYear(year) ? 29 : 28;
+  }
 
-      //   change position for button
-      if (window.innerWidth <= 500) {
-        document.querySelector("button").style.top = "117px";
-      } else {
-        document.querySelector("button").style.top = "87px";
-      }
-
-      label.classList.add("error-label");
-      //   add error to the input
-      formGroupError = true;
-    }
-
-    //   set no error to the input
-    let formGroupError = false;
-    // validation rules that loop through then check the input egainst each of rules and trigger right error message
-    for (const option of validationOptions) {
-      if (input.hasAttribute(option.attribute) && !option.isValid(input)) {
-        errorContainer.textContent = option.errorMessage(input);
-        errorStyling();
-      }
-    }
-
-    //   remove error border and icon from input if here is no error
-    if (!formGroupError) {
-      //   input.classList.remove("border-danger-subtle");
-      label.classList.remove("error-label");
-      errorContainer.textContent = "";
-    }
-
-    // Check for valid day based on the month
-    if (input.id === "day" && input.value) {
-      const day = parseInt(input.value);
-      const month = parseInt(document.getElementById("month").value);
-
-      // Check if day is within valid range for the selected month
-      if (day < 1 || day > getDaysInMonth(month)) {
-        errorContainer.textContent = `Must be a valid date`;
-        errorStyling();
-      }
-    }
-  };
-
-  // Function to get the number of days in a month, considering leap years
-  const getDaysInMonth = (month) => {
-    const leapYear = (year) =>
-      (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-    const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    // Adjust February's days for leap years
-    if (month === 2) {
-      const year = parseInt(document.getElementById("year").value);
-      return leapYear(year) ? 29 : 28;
-    }
-
-    return daysInMonth[month - 1];
-  };
-
-  // disabling HTML standart validation
-  formElement.setAttribute("novalidate", "");
-
-  //adding our validation
-  formElement.addEventListener("submit", (event) => {
-    // prevent page reload after submit
-    event.preventDefault();
-    // go to validation all inputs
-    validateAllFormGroups(formElement);
-  });
-
-  // validate all inputs
-  const validateAllFormGroups = (formToValidate) => {
-    //   create array from all inputs
-    const formGroups = Array.from(
-      formToValidate.querySelectorAll(".input-control")
-    );
-
-    // variable for check error for all forms together
-    let allFormGroupsValid = true;
-
-    //   loop through array
-    formGroups.forEach((formGroup) => {
-      validateSingleFormGroup(formGroup);
-
-      if (formGroup.querySelector(".error").textContent) {
-        allFormGroupsValid = false;
-      }
-    });
-
-    // if no error in forms, call calculateAge()
-    if (allFormGroupsValid) {
-      const input = document.querySelectorAll("input");
-      input.forEach((input) => {
-        input.classList.remove("border-danger-subtle");
-      });
-
-      calculateAge();
-    }
-  };
-};
-
-// select form id
-validateForm("#form");
+  return daysInMonth[month - 1];
+}
